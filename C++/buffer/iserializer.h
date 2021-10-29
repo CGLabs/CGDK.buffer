c3a525de4c711358e32834aa7b3e2e98d6c94013
sangduck <sangduck@cgcii.co.kr>
2021-10-29 19:07:45 +0900

    basic_1

---
+++ C++/buffer/iserializer.h
@@ -0,0 +1,127 @@
+﻿//*****************************************************************************
+//*                                                                           *
+//*                             CGDK::buffer_view                             *
+//*                      Ver 2.0pre / Release 2015.01.05                      *
+//*                                                                           *
+//*                                                                           *
+//*                                                                           *
+//*                                                                           *
+//*  This Program is programmed by Cho SangHyun. sangduck@cgcii.com           *
+//*  Best for Game Developement and Optimized for Game Developement.          *
+//*                                                                           *
+//*                   (C) 2008 CGLabs All right reserved.                     *
+//*                          http://www.CGCII.co.kr                           *
+//*                                                                           *
+//*****************************************************************************
+
+#pragma once
+
+namespace CGDK
+{
+//-----------------------------------------------------------------------------
+//
+// CGDK::buffer_base
+//
+//-----------------------------------------------------------------------------
+template <class ELEMENT_T = char>
+class iserializaer
+{
+// traits) 
+protected:
+			using traits		 = _buffer_traits_t<ELEMENT_T>;
+			using element_t		 = ELEMENT_T;
+			using size_type		 = std::size_t;
+
+public:
+	// 2) prepend																								  
+			template <std::size_t ISIZE>
+	constexpr auto				prepend()																		{ return _prepend_skip(ISIZE);}
+			template <class T>																					  
+	constexpr T&				prepend()																		{ return *reinterpret_cast<T*>(_prepend_skip(sizeof(T)));}
+			template <class T>																					  
+	constexpr prpd_tr<T>		prepend(const T& _data)															{ return PRPD_t<basic_buffer, T>::_do_prepend(*this, _data);;}
+			template <class T>																					  
+	constexpr prpd_tr<T>		prepend(T&& _data)																{ return PRPD_t<basic_buffer, T>::_do_prepend(*this, _data);;}
+	//		template <class T, std::size_t N>
+	//constexpr typename serializer_prepend<T[N]>::type prepend(const T (&_data)[N])								{ return _prepend(_data);}
+			template <class T> 																					  
+	constexpr base_t			prepend(const T* _data, std::size_t _count)										{ return _prepend_array(_data, _count);}
+			template <class T> 																					  
+	constexpr auto				prepend(const std::initializer_list<T>& _list)									{ _prepend_initializer_list<T>(_list); return data();}
+			template<class I> 																					  
+	constexpr std::enable_if_t<is_iterator<I>::value, base_t>
+								prepend(I _first, I _last)														{ return _prepend_iterator(_first, _last);}
+			auto				prepend(std::size_t _size, const void* _buffer)									{ return _prepend_bytes(_size, _buffer);}
+			template <class T>
+	constexpr std::enable_if_t<is_string_type<T>::value, base_t>
+								prepend_string(std::basic_string_view<T> _text)									{ return _prepend_string(_text);}
+	//		template <class T, std::size_t N>
+	//constexpr std::enable_if_t<is_string_type<T>::value, base_t>
+	//							prepend_string(const T(&_text)[N])												{ return _prepend_string_pointer(_text);}
+			template <class T, class F, class... TREST>
+	constexpr std::enable_if_t<is_string_type<T>::value, base_t>
+								prepend_string(const std::basic_string_view<T> _format, F&& _first, TREST&&... _rest)	{ return _prepend_string_format(_format.data(), std::forward<F>(_first), std::forward<TREST>(_rest)...);}
+			template <class T, std::size_t N, class F, class... TREST>
+	constexpr std::enable_if_t<is_string_type<T>::value, base_t>
+								prepend_string(const T(&_format)[N], F&& _first, TREST&&... _rest)				{ return _prepend_string_format(_format, std::forward<F>(_first), std::forward<TREST>(_rest)...);}
+			template <class T>
+	constexpr std::enable_if_t<is_string_type<T>::value, base_t>
+								prepend_text(std::string_view _text)											{ return _prepend_text(_text);}
+			template <class T, std::size_t N>
+	constexpr std::enable_if_t<is_string_type<T>::value, base_t>
+								prepend_text(const T(&_text)[N])												{ return _prepend_text(_text);}
+			template <class T, class F, class... TREST>
+	constexpr std::enable_if_t<is_string_type<T>::value, base_t>
+								prepend_text(std::basic_string_view<T> _format, F&& _first, TREST&&... _rest)	{ return _prepend_text_format(_format, std::forward<F>(_first), std::forward<TREST>(_rest)...);}
+			template <class T, std::size_t N, class F, class... TREST>
+	constexpr std::enable_if_t<is_string_type<T>::value, base_t>
+								prepend_text(const T(&_format)[N], F&& _first, TREST&&... _rest)				{ return _prepend_text_format(_format, std::forward<F>(_first), std::forward<TREST>(_rest)...);}
+
+	// 3) append
+			template <std::size_t ISIZE>
+	constexpr auto&				append()																		{ return _append_skip(ISIZE);}
+			template <class T>																					  
+	constexpr auto&				append()																		{ return _append_emplace<std::remove_reference_t<std::remove_const_t<T>>>();}
+			template <class T>																					  
+	constexpr appd_tr<T>		append(const T& _data)															{ return APPD_t<basic_buffer,T>::_do_append(*this, _data); }
+			template <class T>																					  
+	constexpr appd_tr<T>		append(T&& _data)																{ return APPD_t<basic_buffer,T>::_do_append(*this, std::forward<T>(_data)); }
+			template <class T>																					  
+	constexpr auto				append(const T* _data, std::size_t _count)										{ return _append_array(_data, _count);}
+			template <class T> 																					  
+	constexpr base_t			append(const std::initializer_list<T>& _List)									{ return _append_initializer_list<T>(_List);}
+			template<class I> 																					  
+	constexpr std::enable_if_t<is_iterator<I>::value, base_t>
+								append(I _first, I _last)														{ return _append_iterator(_first, _last);}
+	constexpr auto				append(std::size_t _size, const void* _buffer)									{ return _append_bytes(_size, _buffer); }
+
+			template <class T, class F, class... TREST>
+	constexpr std::enable_if_t<is_string_type<T>::value, base_t>
+								append(std::basic_string_view<T> _format, F&& _first, TREST&&... _rest)			{ return _append_string_format(_format, std::forward<F>(_first), std::forward<TREST>(_rest)...);}
+			template <class T, std::size_t N, class F, class... TREST>
+	constexpr std::enable_if_t<is_string_type<T>::value, base_t>
+								append(const T(&_format)[N], F&& _first, TREST&&... _rest)						{ return _append_string_format(_format, std::forward<F>(_first), std::forward<TREST>(_rest)...);}
+			template <class T>
+	constexpr std::enable_if_t<is_string_type<T>::value, base_t>
+								append_text(std::basic_string_view<T> _text)									{ return _append_text(_text);}
+
+			template <class T, std::size_t N>
+	constexpr std::enable_if_t<is_string_type<T>::value, base_t>
+								append_text(const T(&_text)[N])													{ return _append_text(_text);}
+			template <class T, class F, class... TREST>
+	constexpr std::enable_if_t<is_string_type<T>::value, base_t>
+								append_text(std::basic_string_view<T> _format, F&& _first, TREST&&... _rest)	{ return _append_text_fmt_format(_format, std::forward<F>(_first), std::forward<TREST>(_rest)...);}
+			template <class T, std::size_t N, class F, class... TREST>
+	constexpr std::enable_if_t<is_string_type<T>::value, base_t>
+								append_text(const T(&_format)[N] , F&& _first, TREST&&... _rest)				{ return _append_text_fmt_format(_format, std::forward<F>(_first), std::forward<TREST>(_rest)...);}
+
+			template <class T = char, class TNUM>
+	constexpr std::enable_if_t<std::is_integral_v<TNUM>, base_t>
+				 				append_text(TNUM _value)														{ return _append_text_integral<T>(_value);}
+			template <class T = char, class TNUM>
+	constexpr std::enable_if_t<std::is_floating_point_v<TNUM>, base_t>
+				 				append_text(TNUM _value)														{ return _append_text_float<T>(_value);}
+};
+
+
+}
---
+++ C++/buffer/oserializer.h
@@ -0,0 +1,117 @@
+﻿//*****************************************************************************
+//*                                                                           *
+//*                             CGDK::buffer_view                             *
+//*                      Ver 2.0pre / Release 2015.01.05                      *
+//*                                                                           *
+//*                                                                           *
+//*                                                                           *
+//*                                                                           *
+//*  This Program is programmed by Cho SangHyun. sangduck@cgcii.com           *
+//*  Best for Game Developement and Optimized for Game Developement.          *
+//*                                                                           *
+//*                   (C) 2008 CGLabs All right reserved.                     *
+//*                          http://www.CGCII.co.kr                           *
+//*                                                                           *
+//*****************************************************************************
+
+#pragma once
+
+namespace CGDK
+{
+//-----------------------------------------------------------------------------
+//
+// CGDK::buffer_base
+//
+//-----------------------------------------------------------------------------
+template <class ELEMENT_T = char>
+class oserializaer
+{
+// traits) 
+protected:
+			using traits		 = _buffer_traits_t<ELEMENT_T>;
+			using element_t		 = ELEMENT_T;
+			using size_type		 = std::size_t;
+
+public:
+	// 3) extract/subtract
+			template <std::size_t ISIZE>
+	constexpr auto				extract()								{ return _extract_bytes(ISIZE);}
+	constexpr auto				extract(CGDK::size _length)				{ return _extract_bytes(_length.amount);}
+	constexpr auto				extract(CGDK::skip _length)				{ return _extract_bytes(_length.amount);}
+	constexpr auto				extract(size_type _size, void* _buffer)	{ return _extract_bytes(_size, _buffer);}
+			template <class T>
+	constexpr std::enable_if_t<!std::is_reference_v<T>, extr_tr<T>>
+								extract()								{ return serializer_extract<_buffer_view, std::remove_const_t<T>>::_do_extract(*this);}
+			template <class T>
+	constexpr std::enable_if_t<!std::is_reference_v<T>, void>
+								extract_to(T& _dest)					{ serializer_extract<_buffer_view, std::remove_const_t<T>>::_do_extract(_dest, *this);}
+			template <class T>
+	constexpr std::enable_if_t<std::is_reference_v<T>, T>
+								extract()								{ return _extract_general<std::remove_reference_t<std::remove_const_t<T>>>();}
+			template <class T>
+	constexpr std::enable_if_t<std::is_reference_v<T>, void>
+								extract(T& _dest)						{ _extract_general<std::remove_reference_t<std::remove_const_t<T>>>(_dest);}
+			template <class T>
+	constexpr auto				extract_string(T* _string, size_type _length_in_words=(size_type(-1))) { return _extract_string_copy(_string, _length_in_words);}
+			template<class... T>
+	constexpr _buffer_view		extract(const std::tuple<T...>& _tuple)	{ auto buf_old = this->data_; _extract_tuple(_tuple); return _buffer_view(buf_old, this->data_ - buf_old);}
+			template<class... T>
+	constexpr _buffer_view		extract(std::tuple<T...>&& _tuple)		{ auto buf_old = this->data_; _extract_tuple(std::move(_tuple)); return _buffer_view(buf_old, this->data_ - buf_old);}
+	constexpr std::string_view	extract_web_modify()					{ return _extract_web_modify();}
+			template <class T = char>
+	constexpr std::enable_if_t<is_string_type<T>::value, std::basic_string_view<T>>
+								extract_text(T _terminal = 0)			{ return _extract_text(_terminal);}
+			template <class T = char>
+	constexpr std::enable_if_t<is_string_type<T>::value, std::basic_string_view<T>>
+								extract_text(T _terminal, T _modify)	{ return _extract_text(_terminal, _modify);}
+			template <class T = char>
+	constexpr std::enable_if_t<is_string_type<T>::value, std::basic_string_view<T>>
+								extract_text(CGDK::size _length)		{ return _extract_text<T>(_length.amount);}
+
+			template <class T>
+	constexpr auto&				subtract()								{ return _subtract_general<T>();}
+	constexpr auto				subtract(size_type _length)				{ return _subtract_bytes(_length);}
+
+	// 4) front
+	constexpr auto				front(int64_t _offset = 0) const		{ return _front(_offset);}
+			template <class T>
+	constexpr std::enable_if_t<!std::is_reference_v<T>, peek_tr<T>>
+								front(int64_t _offset = 0) const		{ return _front<T>(_offset);}
+			template <class T>
+	constexpr std::enable_if_t<std::is_reference_v<T>, T>
+								front(int64_t _offset = 0) const		{ return _front_general<std::remove_reference_t<std::remove_const_t<T>>>(_offset);}
+			template <class T>
+	constexpr std::enable_if_t<!std::is_reference_v<T>, void>
+								front_to(T& _dest, int64_t _offset = 0) const { _front<T>(_dest, _offset);}
+			template <class T>
+	constexpr std::enable_if_t<std::is_reference_v<T>, void>
+								front_to(T& _dest, int64_t _offset = 0) const { _front_general<std::remove_reference_t<std::remove_const_t<T>>>(_dest, _offset);}
+			template <class T>
+	constexpr std::enable_if_t<!std::is_reference_v<T>, peek_tr<T>>
+								front(POS& _pos) const					{ return _front<T>(_pos.offset);}
+			template <class T>
+	constexpr std::enable_if_t<std::is_reference_v<T>, T>
+								front(POS& _pos) const					{ return _front_general<std::remove_reference_t<std::remove_const_t<T>>>(_pos.offset);}
+			template <class T>
+	constexpr std::enable_if_t<!std::is_reference_v<T>, void>
+								front_to(T& _dest, POS& _pos) const		{ _front<T>(_dest, _pos.offset);}
+			template <class T>
+	constexpr std::enable_if_t<std::is_reference_v<T>, void>
+								front_to(T& _dest, POS& _pos) const		{ _front_general<std::remove_reference_t<std::remove_const_t<T>>>(_dest, _pos.offset);}
+			template <class T = char>
+	constexpr std::enable_if_t<is_string_type<T>::value, std::basic_string_view<T>>
+								front_text(T _terminal = 0, int64_t _offset = 0) { return _front(_text<T>(_terminal, _offset));}
+			template <class T = char>
+	constexpr std::enable_if_t<is_string_type<T>::value, std::basic_string_view<T>>
+								front_text(CGDK::size _length, int64_t _offset = 0) { return _front_text_by_length<T>(_length.amount, _offset);}
+
+	// 5) back
+	constexpr auto				back(int64_t _offset = 0) const			{ return _back(_offset);}
+			template <class T>
+	constexpr peek_tr<T>		back(int64_t _offset = 0) const			{ return _back<T>(_offset);}
+			template <class T>
+	constexpr peek_tr<T>		back(POS& _pos) const					{ return _back<T>(_pos.offset);}
+};
+
+
+}
