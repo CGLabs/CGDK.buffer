3da4ca8647c67801b978100331b98c6e90a710bb
sangduck <sangducks@gmail.com>
2023-11-02 04:00:04 +0900

    add property files

---
+++ C#/projects/buffer.NET/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CGDK.buffer.framework")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CGDK.buffer.framework")]
+[assembly: AssemblyCopyright("Copyright ©2023")]
+[assembly: AssemblyTrademark("CGCII")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("7d5b6392-b36f-4b81-b229-ee314090cd8c")]
+[assembly: AssemblyVersion("3.0.2.0")]
+[assembly: AssemblyFileVersion("3.0.2.0")]
