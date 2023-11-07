68b89691ac63634c889f0032e1a7592c0c87080b
sangduck <sangducks@gmail.com>
2023-11-08 03:19:03 +0900

    add flatbuffers generate batch file

---
+++ C++/unit_test/unit_test.benchmark.basic/flatbuffers/generate_messages.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+..\..\vcpkg_installed\x64-windows\x64-windows\tools\flatbuffers\flatc -c messages_test_1.fbs
+..\..\vcpkg_installed\x64-windows\x64-windows\tools\flatbuffers\flatc -c messages_test_2.fbs
+..\..\vcpkg_installed\x64-windows\x64-windows\tools\flatbuffers\flatc -c messages_test_3.fbs
+..\..\vcpkg_installed\x64-windows\x64-windows\tools\flatbuffers\flatc -c messages_test_4.fbs
+..\..\vcpkg_installed\x64-windows\x64-windows\tools\flatbuffers\flatc -c messages_test_5.fbs
